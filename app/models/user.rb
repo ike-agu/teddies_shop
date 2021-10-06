0ab6e4b7512db3f9ca9c8d9e5f32077420706bac
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-10-06 18:02:11 +0100

    added orders controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :orders
+
+  # def premium?
+  #   orders.where
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # root to: 'pages#home'
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'teddies#index'
-resources :teddies, only: [:index, :show]
+  resources :teddies, only: [:index, :show]
+  resources :orders, only: [:show, :create]
 end
