88fd15f7d5d36ad36687734db194682ab19451c7
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-10-06 18:46:17 +0100

    stripe payment succesfully added

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'teddies#index'
   resources :teddies, only: [:index, :show]
-  resources :orders, only: [:show, :create]
+  resources :orders, only: [:show, :create] do
+    resources :payments, only: :new
+  end
 end
